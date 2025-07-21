a7a212f70b43f15db6641bc2a2bf456e71371850
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-07-21 22:10:26 +0530

    updated the mode-toggle name

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ModeToggle } from "../components/ModeToggle/modetoggle";
+import { ModeToggle } from "../components/ModeToggle/mode-toggle";
 
 export default function Home() {
   return (
